a200cca1c8001ce1b70f1becd3750dfe3e280b0d
Maryie <mashenka.zinkevich0245@gmail.com>
2024-10-08 16:49:49 +0300

    The HW4 was fixed

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import s from './Stand.module.css';
-import SuperInputText from './common/c1-SuperInputText/SuperInputText';
-import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox';
-import SuperButton from './common/c2-SuperButton/SuperButton';
+import React, { useState } from 'react'
+import s from './Stand.module.css'
+import SuperInputText from './common/c1-SuperInputText/SuperInputText'
+import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox'
+import SuperButton from './common/c2-SuperButton/SuperButton'
 
 const Stand = () => {
-    const [stateForAllInputs, setValue] = useState<string>('');
-    const [error, setError] = useState<string>('');
-    const [stateForAllCheckboxes, setChecked] = useState<boolean>(false);
+    const [stateForAllInputs, setValue] = useState<string>('')
+    const [error, setError] = useState<string>('')
+
+    const [stateForAllCheckboxes, setChecked] = useState<boolean>(false)
 
     return (
         <div id={'hw4-stand'} className={s.stand}>
             <div className={s.inputs}>
+                {/*совместим со старым кодом:*/}
                 <div>
                     <SuperInputText
                         id={'hw4-super-input-like-old'}
@@ -19,6 +21,7 @@ const Stand = () => {
                         onChange={(e) => setValue(e.currentTarget.value)}
                     />
                 </div>
+                {/*инпут с ошибкой:*/}
                 <div>
                     <SuperInputText
                         id={'hw4-super-input-with-error'}
@@ -26,24 +29,31 @@ const Stand = () => {
                         onChangeText={setValue}
                         error={error}
                         onEnter={() => {
-                            setError(stateForAllInputs.trim() ? '' : 'Error');
-                            setValue('');
+                            setError(
+                                stateForAllInputs.trim()
+                                    ? ''
+                                    : 'Error'
+                            )
+                            setValue('')
                         }}
                     />
                 </div>
             </div>
 
             <div className={s.buttons}>
+                {/*обычная кнопка:*/}
                 <div>
                     <SuperButton id={'hw4-super-button-default'}>
                         default
                     </SuperButton>
                 </div>
+                {/*красная кнопка:*/}
                 <div>
                     <SuperButton id={'hw4-super-button-red'} xType={'red'}>
                         red
                     </SuperButton>
                 </div>
+                {/*задизэйбленная кнопка:*/}
                 <div>
                     <SuperButton
                         id={'hw4-super-button-disabled'}
@@ -53,6 +63,7 @@ const Stand = () => {
                         disabled
                     </SuperButton>
                 </div>
+                {/*задизэйбленная кнопка:*/}
                 <div>
                     <SuperButton
                         id={'hw4-super-button-secondary'}
@@ -64,6 +75,7 @@ const Stand = () => {
             </div>
 
             <div className={s.checkboxes}>
+                {/*чекбокс с текстом:*/}
                 <div>
                     <SuperCheckbox
                         id={'hw4-super-checkbox-with-text'}
@@ -73,6 +85,7 @@ const Stand = () => {
                         some text
                     </SuperCheckbox>
                 </div>
+                {/*совместим со старым кодом:*/}
                 <div>
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
@@ -82,7 +95,7 @@ const Stand = () => {
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Stand;
+export default Stand
--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -2,31 +2,38 @@ import React, {
     ChangeEvent,
     DetailedHTMLProps,
     InputHTMLAttributes,
-} from 'react';
-import s from './SuperCheckbox.module.css';
+} from 'react'
+import s from './SuperCheckbox.module.css'
 
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+// тип пропсов обычного инпута
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement>
 
 type SuperCheckboxPropsType = Omit<DefaultInputPropsType, 'type'> & {
-    onChangeChecked?: (checked: boolean) => void;
-    spanClassName?: string;
-};
+    onChangeChecked?: (checked: boolean) => void
+    spanClassName?: string
+}
 
-const SuperCheckbox: React.FC<SuperCheckboxPropsType> = ({
-    onChange,
-    onChangeChecked,
-    className,
-    spanClassName,
-    children,
-    id,
-    ...restProps
-}) => {
+const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
+    {
+        onChange,
+        onChangeChecked,
+        className,
+        spanClassName,
+        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
+        id,
+
+        ...restProps // все остальные пропсы попадут в объект restProps
+    }
+) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeChecked?.(e.currentTarget.checked);
-        onChange?.(e);
-    };
+        //onChangeChecked?. если он передан то вызовется
+        onChangeChecked?.(e.currentTarget.checked) //получаю свойство checked
+        onChange?.(e)
+    }
 
-    const finalInputClassName = `${s.checkbox} ${className || ''}`;
+    const finalInputClassName = s.checkbox
+        + (className ? ' ' + className : '')
 
     return (
         <label className={s.label}>
@@ -35,15 +42,18 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = ({
                 type={'checkbox'}
                 onChange={onChangeCallback}
                 className={finalInputClassName}
-                {...restProps}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
             />
             {children && (
-                <span id={id ? id + '-span' : undefined} className={s.spanClassName}>
+                <span
+                    id={id ? id + '-span' : undefined}
+                    className={s.spanClassName}
+                >
                     {children}
                 </span>
             )}
-        </label>
-    );
-};
+        </label> // благодаря label нажатие на спан передастся в инпут
+    )
+}
 
-export default SuperCheckbox;
+export default SuperCheckbox
