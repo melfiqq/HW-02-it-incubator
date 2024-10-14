9d69a530efc8958b2d6535805099677546732cba
Maryie <mashenka.zinkevich0245@gmail.com>
2024-10-14 15:14:10 +0300

    HW5 was made

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,15 +18,19 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path = "/" element = {<Navigate to = {PATH.PRE_JUNIOR}/>} /> {/*  "/" is placed only in the parent line.  */}
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+
+                <Route path = {PATH.PRE_JUNIOR} element = {<PreJunior />} />
+
+                <Route path = {PATH.JUNIOR} element = {<Junior />} />
+
+                <Route path = {PATH.JUNIOR_PLUS} element = {<JuniorPlus />} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+
+                <Route path = "*" element = {<Error404 />} /> {/* "*" means that Error404 will appear in case of any other page will not work. */}
             </Routes>
         </div>
     )
